b56ed73276bd0014716f772f1eb79729890265e6
Kuzegor <kuznetcov03@inbox.ru>
2022-11-16 19:41:36 +0300

    PositiveToNegativeConverter

---
+++ AnimatedUI/AnimatedUI/PositiveToNegativeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AnimatedUI
+{
+    internal class PositiveToNegativeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double output = -(double)value;
+            return output;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}

479042a0f48925c30564800aa89331e16b48c63c
Kuzegor <kuznetcov03@inbox.ru>
2022-11-25 15:18:40 +0300

    More Views

--- AnimatedUI/AnimatedUI/ViewModels/MenuStripViewModel.cs
+++ AnimatedUI/AnimatedUI/ViewModels/MenuStripViewModel.cs
@@ -25,12 +25,21 @@ namespace AnimatedUI.ViewModels
         private HomeViewModel HomeViewModel { get; set; }
         private AccountViewModel AccountViewModel { get; set; }
         private SettingsViewModel SettingsViewModel { get; set; }
+        private StatisticViewModel StatisticViewModel { get; set; }
+        private InfoViewModel InfoViewModel { get; set; }
+        private MailViewModel MailViewModel { get; set; }
+        private OffersViewModel OffersViewModel { get; set; }
+
 
         public MenuStripViewModel()
         {
             HomeViewModel = new HomeViewModel();
             AccountViewModel = new AccountViewModel();
             SettingsViewModel = new SettingsViewModel();
+            StatisticViewModel = new StatisticViewModel();
+            InfoViewModel = new InfoViewModel();
+            MailViewModel = new MailViewModel();
+            OffersViewModel = new OffersViewModel();
             CurrentViewModel = HomeViewModel;
 
             ShowHomeViewCommand = new DelegateCommand(x => 
@@ -60,10 +69,51 @@ namespace AnimatedUI.ViewModels
                 }));
                 thread.Start();
             });
+            ShowOffersViewCommand = new DelegateCommand(x =>
+            {
+                Thread thread = new Thread(new ThreadStart(() =>
+                {
+                    Thread.Sleep(700);
+                    Instance.CurrentViewModel = OffersViewModel;
+                }));
+                thread.Start();
+            });
+            ShowStatisticViewCommand = new DelegateCommand(x =>
+            {
+                Thread thread = new Thread(new ThreadStart(() =>
+                {
+                    Thread.Sleep(700);
+                    Instance.CurrentViewModel = StatisticViewModel;
+                }));
+                thread.Start();
+            });
+            ShowMailViewCommand = new DelegateCommand(x =>
+            {
+                Thread thread = new Thread(new ThreadStart(() =>
+                {
+                    Thread.Sleep(700);
+                    Instance.CurrentViewModel = MailViewModel;
+                }));
+                thread.Start();
+            });
+            ShowInfoViewCommand = new DelegateCommand(x =>
+            {
+                Thread thread = new Thread(new ThreadStart(() =>
+                {
+                    Thread.Sleep(700);
+                    Instance.CurrentViewModel = InfoViewModel;
+                }));
+                thread.Start();
+            });
         }
 
         public DelegateCommand ShowHomeViewCommand { get; set; }
         public DelegateCommand ShowAccountViewCommand { get; set; }
         public DelegateCommand ShowSettingsViewCommand { get; set; }
+        public DelegateCommand ShowStatisticViewCommand { get; set; }
+        public DelegateCommand ShowInfoViewCommand { get; set; }
+        public DelegateCommand ShowMailViewCommand { get; set; }
+        public DelegateCommand ShowOffersViewCommand { get; set; }
+
     }
 }
